ac8e9d71c43fedd5877245007f638a55f8230cce
Alex Keith <alexmkeith13@gmail.com>
2018-03-27 13:24:05 -0400

    got both fragments

---
+++ app/src/main/java/com/example/alexkeith/triviaapp/QuestionCreatorFragment.java
@@ -0,0 +1,41 @@
+package com.example.alexkeith.triviaapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.alexkeith.triviaapp.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QuestionCreatorFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QuestionCreatorFragment extends Fragment {
+
+
+    // TODO: Rename and change types and number of parameters
+    public static QuestionCreatorFragment newInstance() {
+        QuestionCreatorFragment fragment = new QuestionCreatorFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_question_creator, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+}
